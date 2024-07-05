2c40ce662861298ea86aef7d8b8fd480fbdc52d9
Viktor Bezuglov <viktory683@gmail.com>
2024-07-06 03:22:44 +0600

    refactor!: rework socket handling and daemon mode
    - Bumped version from `0.4.12-alpha.1` to `0.5.0-alpha` in Cargo.toml.
    - Added `daemon` field to Config struct.
    - Introduced `Toggle` and `Close` commands.
    - Replaced `Daemon` command with a new logic for showing, hiding, toggling, and closing the application.
    - Moved `SOCKET_BUF_SIZE` to config.rs and added `socket_path` function for consistent socket path handling.
    - Refactored main logic to handle daemon mode and socket commands more effectively.
    - Adjusted activation setup to remove redundant window references.
    - Updated `setup_main_window` and `setup_entry` functions to simplify their implementations.
    - Enhanced key press event handling to use new command-based approach.
    - Updated config example to include the new `daemon` setting with warnings for NixOS users.

--- anyrun/src/main.rs
+++ anyrun/src/main.rs
@@ -22,16 +22,14 @@ use plugins::*;
 use ui::*;
 use utils::*;
 
-const SOCKET_BUF_SIZE: usize = 1024;
-
 fn send_command(command: &str) {
     use std::io::Write;
 
-    let socket_path = glib::tmp_dir().join(format!("{}.sock", APP_ID));
+    let socket_path = socket_path();
 
-    if let Ok(mut stream) = UnixStream::connect(socket_path) {
-        debug!("Connected to socket");
-        debug!("Sending: \"{}\"", command);
+    if let Ok(mut stream) = UnixStream::connect(socket_path.clone()) {
+        debug!("Connected to socket: {}", socket_path.to_string_lossy());
+        debug!("Sending: {:?}", command);
         stream.write_all(command.as_bytes()).unwrap();
     } else {
         error!("Failed to connect to socket. Is it running?");
@@ -46,15 +44,21 @@ fn main() -> Result<glib::ExitCode, glib::Error> {
     app.register(gio::Cancellable::NONE)?;
 
     let args = Args::parse();
+    let config_dir = determine_config_dir(&args.config_dir);
+    let (mut config, error_label) = load_config(&config_dir);
+    config.merge_opt(args.config);
 
     if app.is_remote() {
         debug!("More than one instance running. We are remote");
         if let Some(command) = args.command {
             match command {
-                Command::Daemon => error!("You can't run it as daemon as there we are remote"),
                 Command::Show => send_command("show"),
                 Command::Hide => send_command("hide"),
+                Command::Toggle => send_command("toggle"),
+                Command::Close => send_command("close"),
             }
+        } else {
+            send_command("show")
         }
 
         return Ok(glib::ExitCode::SUCCESS);
@@ -62,7 +66,7 @@ fn main() -> Result<glib::ExitCode, glib::Error> {
 
     debug!("Running as main instance");
 
-    let socket_path = glib::tmp_dir().join(format!("{}.sock", APP_ID));
+    let socket_path = socket_path();
     if socket_path.exists() {
         std::fs::remove_file(socket_path.clone()).unwrap();
     }
@@ -78,22 +82,23 @@ fn main() -> Result<glib::ExitCode, glib::Error> {
         .expect("Failed to add address to the service");
     debug!("Created socket at {}", socket_path.to_string_lossy());
 
+    let daemon = config.daemon;
+
     service.connect_incoming(
         clone!(@weak app => @default-return true, move |_, connection, _| {
             debug!("NEW INCOME");
-            let istream = connection.input_stream();
-            let mut buf = [0; SOCKET_BUF_SIZE];
-            let count = istream.read(&mut buf, gio::Cancellable::NONE);
-            let what_we_got = std::str::from_utf8(&buf).unwrap().trim_matches(char::from(0));
-            debug!("({:?})> {}", count, what_we_got);
+            let command = read_from_stream(&connection.input_stream());
+            debug!("> {:?}", command);
 
             let windows = app.windows();
-            let w = windows.first();
-            if let Some(w) = w {
-                match what_we_got {
-                    "show" => w.show(),
-                    "hide" => w.hide(),
-                    _ => error!("Unknown command: {:?}", what_we_got),
+            let window = windows.first();
+            if let Some(window) = window {
+                match command.as_str() {
+                    "show" => window.show(),
+                    "hide" => if daemon {window.hide()} else {send_command("close")},
+                    "toggle" => if window.is_visible() {send_command("hide")} else {send_command("show")},
+                    "close" => window.close(),
+                    _ => error!("Unknown command received: {:?}", command),
                 }
             }
             true
@@ -104,10 +109,6 @@ fn main() -> Result<glib::ExitCode, glib::Error> {
     debug!("Service started");
 
     let app_state = gio::Settings::new(APP_ID);
-
-    let config_dir = determine_config_dir(&args.config_dir);
-    let (mut config, error_label) = load_config(&config_dir);
-    config.merge_opt(args.config);
 
     let display = gdk::Display::default().expect("No display found");
     let monitor = display
@@ -147,7 +148,7 @@ fn main() -> Result<glib::ExitCode, glib::Error> {
     app.connect_activate(
         clone!(@weak runtime_data => move |app| activate(app, runtime_data.clone())),
     );
-    let exit_code = app.run_with_args::<String>(&[]);
+    let exit_code = app.run();
 
     handle_post_run_action(runtime_data);
 
@@ -179,7 +180,7 @@ fn activate(app: &impl IsA<gtk::Application>, runtime_data: Rc<RefCell<RuntimeDa
 
     let window = setup_main_window(app, runtime_data.clone());
 
-    let entry = setup_entry(window.clone(), runtime_data.clone());
+    let entry = setup_entry(runtime_data.clone());
 
     if runtime_data.borrow().config.save_entry_state {
         let app_state = runtime_data.borrow().app_state.clone();
@@ -193,12 +194,7 @@ fn activate(app: &impl IsA<gtk::Application>, runtime_data: Rc<RefCell<RuntimeDa
         }),
     );
 
-    setup_activation(
-        entry.clone(),
-        main_list.clone(),
-        window.clone(),
-        runtime_data.clone(),
-    );
+    setup_activation(entry.clone(), main_list.clone(), runtime_data.clone());
 
     if runtime_data.borrow().config.show_results_immediately {
         refresh_matches(&entry.text(), runtime_data.clone());
@@ -211,5 +207,7 @@ fn activate(app: &impl IsA<gtk::Application>, runtime_data: Rc<RefCell<RuntimeDa
         main_list.clone(),
     );
 
-    window.present();
+    if !runtime_data.borrow().config.daemon {
+        window.present();
+    }
 }
